a94f4d576dff36d9bae334ba75d279ceade98ebe
thititongumpun <thiti180536@gmail.com>
2023-09-28 22:15:27 +0700

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse('favicon.ico')
